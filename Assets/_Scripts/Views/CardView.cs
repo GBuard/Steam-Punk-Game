d5b8da42ab835c005de35958f35553a8d1862b77
GBuard <guenolebuard.pro@gmail.com>
2026-04-21 10:45:10 +0200

    Initial commit

---
+++ Assets/_Scripts/Views/CardView.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text title;
+
+    [SerializeField] private TMP_Text description;
+
+    [SerializeField] private TMP_Text mana;
+
+    [SerializeField] private SpriteRenderer imageSR;
+
+    [SerializeField] private GameObject wrapper;
+
+    public Card Card {  get; private set; }
+    public void Setup(Card card)
+    {
+        Card = card;
+        title.text = card.Title;
+        description.text = card.Description;
+        mana.text = card.Mana.ToString();
+        imageSR.sprite = card.Image;
+    }
+}
