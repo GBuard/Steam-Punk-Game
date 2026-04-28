1170de56bb3dd535b42fea20eab296152855debf
GBuard <guenolebuard.pro@gmail.com>
2026-04-28 10:33:14 +0200

    Card hover + Game actions

--- Assets/_Scripts/Views/CardView.cs
+++ Assets/_Scripts/Views/CardView.cs
@@ -22,4 +22,16 @@ public class CardView : MonoBehaviour
         mana.text = card.Mana.ToString();
         imageSR.sprite = card.Image;
     }
+
+    void OnMouseEnter()
+    {
+        wrapper.SetActive(false);
+        Vector3 pos = new(transform.position.x, -1, 0);
+        CardViewHoverSystem.Instance.Show(Card, pos);
+    }
+    void OnMouseExit()
+    { 
+        CardViewHoverSystem.Instance.Hide();
+        wrapper.SetActive(true);
+    }
 }
